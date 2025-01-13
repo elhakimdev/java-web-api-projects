e23fc8ccdfa667a571d0e4031a9b9d5e2891cbc1
ATS - Elhakim Cool <bagusajieiswara30@gmail.com>
2025-01-13 15:51:00 +0700

    fix: fix using oracle ucp connection pool

---
+++ src/main/java/com/sass/erp/finance/cash/api_service/models/entities/AccountGroupEntity.java
@@ -0,0 +1,21 @@
+package com.sass.erp.finance.cash.api_service.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "CASH_COMMONS_MASTERS_ACCOUNT_GROUPS")
+public class AccountGroupEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID) // Generates UUID automatically
+    private UUID id;
+}
