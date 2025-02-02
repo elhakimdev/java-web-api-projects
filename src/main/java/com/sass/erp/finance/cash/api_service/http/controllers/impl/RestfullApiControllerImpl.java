06221f1dee34fa201ebfa33795ef7772bb8e9777
ATS - Elhakim Cool <bagusajieiswara30@gmail.com>
2025-02-03 02:44:56 +0700

    fix: fixing add docs api

--- src/main/java/com/sass/erp/finance/cash/api_service/http/controllers/impl/RestfullApiControllerImpl.java
+++ src/main/java/com/sass/erp/finance/cash/api_service/http/controllers/impl/RestfullApiControllerImpl.java
@@ -56,18 +56,22 @@ public abstract class RestfullApiControllerImpl<
   // Abstract method for defining entity class
   protected abstract Class<T> getEntityClass();
 
+  // Hook will be fired before saved entity persisted into db;
   protected T beforeStore(T entity) {
     return entity;
   }
 
+  // Hook will be fired after saved entity persisted into db;
   protected T afterStore(T entity) {
     return entity;
   }
 
+  // Hook will be fired before updated entity persisted into db;
   protected T beforeUpdate(T entity) {
     return entity;
   }
 
+  // Hook will be fired after updated entity persisted into db;
   protected T afterUpdate(T entity) {
     return entity;
   }
@@ -79,14 +83,17 @@ public abstract class RestfullApiControllerImpl<
     @RequestParam(defaultValue = "10") int size
   ) {
 
+    // Find all entity with or without pagination constrains;
+    // Default pagination used if no provided;
     Page<T> pageable = this.service.findAll(PageRequest.of(page - 1, size));
 
-    logger.info("{}", pageable.toString());
-
+    // Transform to json response as a resource collections.
     Map<String, Object> paginatedCollectionsResponse = this.resource.toPaginatedCollectionsResponse(pageable, this.request);
 
+    // Transform to json response as a resources.
     RestfullApiResponse<Map<String, Object>, Object> response = RestfullApiResponseFactory.success(paginatedCollectionsResponse, "List " + this.getResourceCollectionsName(), HttpStatus.OK);
 
+    // Return HTTP Response as json.
     return ResponseEntity.status(response.getStatusCode()).body(response);
   }
 
@@ -95,11 +102,16 @@ public abstract class RestfullApiControllerImpl<
   public HttpEntity<RestfullApiResponse<AbstractMap<String, Object>, Object>> show(
     @PathVariable String uuid
   ) {
+    // Create and embedded ID instance;
     EmbeddedIdentifier embeddedIdentifier = new EmbeddedIdentifier(UUID.fromString(uuid));
+
+    // Find entity by given embedded instance;
     T byIdentifier = this.service.findByIdentifier(embeddedIdentifier);
 
+    // Transform to json response as a resource;
     RestfullApiResponse<AbstractMap<String, Object>, Object> response = RestfullApiResponseFactory.success(this.resource.toResponse(byIdentifier), "Retrieve " + this.getResourceName(), HttpStatus.OK);
 
+    // Return HTTP Response as json.
     return ResponseEntity.status(response.getStatusCode()).body(response);
   }
 
@@ -113,12 +125,16 @@ public abstract class RestfullApiControllerImpl<
     request.setSearchableBy(getSearchableBy());
     request.setSortableBy(getSortableBy());
 
+    // Search entities based given constraints;
     Page<T> page = this.service.search(request, pageable);
 
+    // Transform query result as a paginated collection response;
     Map<String, Object> paginatedCollectionsResponse = this.resource.toPaginatedCollectionsResponse(page, this.request);
 
+    // Transform to json response as a resource collections.
     RestfullApiResponse<Map<String, Object>, Object> response = RestfullApiResponseFactory.success(paginatedCollectionsResponse, "Search result of " + this.getResourceCollectionsName(), HttpStatus.OK);
 
+    // Return HTTP Response as json.
     return ResponseEntity.status(response.getStatusCode()).body(response);
   }
 
@@ -133,14 +149,16 @@ public abstract class RestfullApiControllerImpl<
     // Run before save entity hooks;
     T beforeStore = this.beforeStore(entity);
 
-    // Save ne entity;
+    // Save a new entity;
     T saved = this.service.save(beforeStore);
 
     // Run after save entity hooks;
     this.beforeStore(entity);
 
+    // Transform to json response as a resources;
     RestfullApiResponse<AbstractMap<String, Object>, Object> response = RestfullApiResponseFactory.success(this.resource.toResponse(saved), "Success saving new " + this.getResourceName(), HttpStatus.OK);
 
+    // Return HTTP Response as json.
     return ResponseEntity.status(response.getStatusCode()).body(response);
   }
 }
